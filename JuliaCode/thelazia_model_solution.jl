28c767f94ae4745472ad3465bdf5d27093a636b7
saul <sauld@cimat.mx>
2020-02-19 19:42:01 -0700

    commit with rkf implemented

--- JuliaCode/thelazia_model_solution.jl
+++ JuliaCode/thelazia_model_solution.jl
@@ -39,61 +39,63 @@ ode_problem = ODEProblem(rhs, u_zero, t_span, p)
 sol = solve(ode_problem)
 t = sol.t
 solDataFrame=[sol.t, sol[1, :], sol[2, :], sol[3, :],
-                sol[4, :], sol[5, :], sol[6, :], sol[7,:], sol[8,:]]
-
+                sol[4, :], sol[5, :], sol[6, :],
+                 sol[7, :], sol[8, :]]
 using CSV
 solDataFrame = DataFrame(solDataFrame)
 p1 = plot(solDataFrame, x=:x1, y=:x2,
     Guide.xlabel("time (days)"),
-    Guide.ylabel("Suceptibles Flyes"))
+    Guide.ylabel("S_fs"), Geom.path)
 p2 = plot(solDataFrame, x=:x1, y=:x3,
     Guide.xlabel("time (days)"),
-    Guide.ylabel("Latent Flyes"))
+    Guide.ylabel("L_f"), Geom.path)
 p3 = plot(solDataFrame, x=:x1, y=:x4,
     Guide.xlabel("time (days)"),
-    Guide.ylabel("Infected Flyes"))
+    Guide.ylabel("I_f"), Geom.path)
 #
 p4 = plot(solDataFrame, x=:x1, y=:x5,
     Guide.xlabel("time (days)"),
-    Guide.ylabel("Suceptibles Cows"))
+    Guide.ylabel("S_c"), Geom.path)
 p5 = plot(solDataFrame, x=:x1, y=:x6,
     Guide.xlabel("time (days)"),
-    Guide.ylabel("Latent Cows"))
+    Guide.ylabel("L_c"), Geom.path)
 p6 = plot(solDataFrame, x=:x1, y=:x7,
     Guide.xlabel("time (days)"),
-    Guide.ylabel("Low parasitic load Infected Cows"))
+    Guide.ylabel("I_cl"), Geom.path)
 p7 = plot(solDataFrame, x=:x1, y=:x8,
         Guide.xlabel("time (days)"),
-        Guide.ylabel("High parasitic load Infected Cows"))
-p8 = plot(solDataFrame, x=:x1, y=:x8,
-                Guide.xlabel("time (days)"),
-                Guide.ylabel("Treated Cows"))
+        Guide.ylabel("I_ch"), Geom.path)
+p8 = plot(solDataFrame, x=:x1, y=:x9,
+            Guide.xlabel("time (days)"),
+            Guide.ylabel("T_c"), Geom.path)
 #
 n_flyes = solDataFrame[:, 2 : 4]
-n_cows = solDataFrame[:, 5 : 7]
+n_cows = solDataFrame[:, 5 : 9]
 #
 time = solDataFrame[:, 1]
 n_flyes = n_flyes.x2[:] + n_flyes.x3[:] + n_flyes.x4[:]
-n_cows = n_cows.x5[:] + n_cows.x6[:] + n_cows.x7[:]
+n_cows = n_cows.x5[:] + n_cows.x6[:] + n_cows.x7[:] +
+        + n_cows.x8[:] + n_cows.x9[:]
 #
 #
-p7 = plot(x=time, y=n_flyes,
+p9 = plot(x=time, y=n_flyes,
     Guide.xlabel("time (days)"),
-    Guide.ylabel("Flyes CL"))
-p8 = plot(x=time, y=n_cows,
+    Guide.ylabel("Flyes CL"), Geom.path)
+p10 = plot(x=time, y=n_cows,
     Guide.xlabel("time (days)"),
-    Guide.ylabel("Cows CL"))
+    Guide.ylabel("Cows CL"), Geom.path)
 title(hstack(p1, p2, p3), "Flyes")
-title(hstack(p4, p5, p6), "Cows")
+title(hstack(p4, p5, p6, p7, p8), "Cows")
 plt0 = vstack(
         hstack(p1, p2, p3),
         #hstack(p4, p5, p6),
-        hstack(p7)
+        hstack(p9)
         )
 plt1 = vstack(
         # hstack(p1, p2, p3),
-        hstack(p4, p5, p6, p7),
-        hstack(p8)
+        hstack(p4, p5),
+        hstack(p6, p7),
+        hstack(p8, p10)
         )
 img0 = SVG("flyes_disease_dynamics.svg", 19cm, 11.74289cm)
 img1 = SVG("cows_disease_dynamics.svg", 19cm, 11.74289cm)
